df2502ea90e219398790807476873a4902bd276e
Souvik Nath <86612646+Phylake-69@users.noreply.github.com>
2022-10-06 16:34:04 +0530

    gfg prob on kadane's prob

---
+++ Kadane's_Algorithm/Kadane's_Algorithm.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        
+        int maxsum,currsum,i,j;
+        maxsum=INT_MIN;
+        currsum=0;
+        for(i=0;i<n;i++)
+        {
+            currsum=currsum+arr[i];
+            maxsum=max(maxsum,currsum);
+            if(currsum<0){
+                currsum=0;}
+        }
+        return maxsum;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
