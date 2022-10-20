dbdd1b3d55bbf72d611d48e31d52ce6a74a3d607
soumyadeep23 <soumyadeepchanda.work@gmail.com>
2022-10-20 22:48:04 +0530

    Soumya

--- K_Smallest/k_smallest.cpp
+++ K_Smallest/k_smallest.cpp
@@ -1,58 +1,24 @@
-//{ Driver Code Starts
-//Initial function template for C++
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-// } Driver Code Ends
-//User function template for C++
-
-class Solution{
-    public:
-    // arr : given array
-    // l : starting index of the array i.e 0
-    // r : ending index of the array i.e size-1
-    // k : find kth smallest element and return using this function
-    int kthSmallest(int arr[], int l, int r, int k) {
-        //code here
-        // vector<int>v;
-        // for(int i=0;i<=r;i++)
-        //     v.push_back(arr[i]);
-        // sort(v.begin(),v.end());
-        // return v[k-1];
-        
-        priority_queue<int>p;
-        for(int i=0;i<=r;i++)
-            p.push(arr[i]);
-            
-        for(int i=1;i<=r+1-k;i++)
-            p.pop();
-        
-        return p.top();
-        
-    }
-};
-
-//{ Driver Code Starts.
  
+// Function to return K'th smallest element in a given array
+int kthSmallest(int arr[], int N, int K)
+{
+    // Sort the given array
+    sort(arr, arr + N);
+ 
+    // Return k'th element in the sorted array
+    return arr[K - 1];
+}
+ 
+// Driver's code
 int main()
 {
-    int test_case;
-    cin>>test_case;
-    while(test_case--)
-    {
-        int number_of_elements;
-        cin>>number_of_elements;
-        int a[number_of_elements];
-        
-        for(int i=0;i<number_of_elements;i++)
-            cin>>a[i];
-            
-        int k;
-        cin>>k;
-        Solution ob;
-        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
-    }
+    int arr[] = { 12, 3, 5, 7, 19 };
+    int N = sizeof(arr) / sizeof(arr[0]), K = 2;
+ 
+    // Function call
+    cout << "K'th smallest element is "
+         << kthSmallest(arr, N, K);
     return 0;
 }
-// } Driver Code Ends
